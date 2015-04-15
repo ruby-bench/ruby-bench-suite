22e617cd0ba3606673d7b8d18f436b33835d336c
Guo Xiang Tan <tgx_world@hotmail.com>
2015-04-15 17:17:23 +0800

    Add benchmark for `default_scope`.

---
+++ rails/benchmarks/bm_activerecord_finders_all.rb
@@ -0,0 +1,43 @@
+require 'bundler/setup'
+require 'rails'
+require 'active_record'
+
+require_relative 'support/benchmark_rails.rb'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name, :email
+    t.boolean :admin
+    t.timestamps null: false
+  end
+end
+
+class User < ActiveRecord::Base
+  default_scope { where(admin: true) }
+end
+
+admin = true
+
+1000.times do
+  attributes = {
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    email: "foobar@email.com",
+    admin: admin,
+    created_at: Date.today
+  }
+
+  User.create!(attributes)
+
+  admin = !admin
+end
+
+Benchmark.rails("activerecord/sqlite3_finders", time: 10) do
+  User.all
+end
