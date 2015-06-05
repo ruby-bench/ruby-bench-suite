25336e81f3ceb6a090ae4469be8f11d36e3bb2d7
Kasif Gilbert <kasif.gilbert@gmail.com>
2015-06-04 22:59:20 -0400

    Add new benchmark with to_a and a unique index on email

---
+++ rails/benchmarks/bm_activerecord_scope_to_a.rb
@@ -0,0 +1,32 @@
+require 'bundler/setup'
+require 'active_record'
+require_relative 'support/benchmark_rails'
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name, :email
+    t.timestamps null: false
+    t.index :email, unique: true
+  end
+end
+
+class User < ActiveRecord::Base; end
+
+1000.times do |i|
+  User.create!({
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    email: "foobar#{"%03d" % i}@email.com"
+  })
+end
+
+Benchmark.rails("activerecord/#{db_adapter}_scope_to_a", time: 5) do
+  User
+    .where(name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+    .where("email LIKE :email", email: "foobar00%@email.com")
+    .limit(10)
+    .to_a
+end
