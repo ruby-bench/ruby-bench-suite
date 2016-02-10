d010933e1d71836c6a7e121b169f612fc440df7a
Guo Xiang Tan <tgx_world@hotmail.com>
2016-02-10 18:02:30 +0800

    FIX: Remove trailing comma.

--- rails/benchmarks/actioncable_postgres.rb
+++ rails/benchmarks/actioncable_postgres.rb
@@ -8,7 +8,7 @@ PG_FAYE = Faye::WebSocket::Client.new("ws://127.0.0.1:4001/")
 
 pg_tcp_addr = ENV['POSTGRES_PORT_5432_TCP_ADDR'] || 'localhost'
 pg_port = ENV['POSTGRES_PORT_5432_TCP_PORT'] || 5432
-PG_DB_URL = "postgres://postgres@#{pg_tcp_addr}:#{pg_port}/rubybench",
+PG_DB_URL = "postgres://postgres@#{pg_tcp_addr}:#{pg_port}/rubybench"
 
 Benchmark.rails("actioncable_postgres", time: 10) do
   pg_config =  { adapter: 'postgresql', url: PG_DB_URL }.with_indifferent_access
