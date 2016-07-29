cad09568e60e8752203c8eccfb8a8a5aa82be19f
hiren mistry <hiren.mistry@chai-monsters.com>
2016-07-29 00:01:59 -0700

    Used bundle package before running benchmarks to eliminate network times. Created one Gemfile with no version specified to test the resolver on install and created another Gemfile with versions specified to have a more consistent comparison across bundler versions.

--- bundler/benchmarks/bm_bundle_install_rails_fixed.rb
+++ bundler/benchmarks/bm_bundle_install_rails_fixed.rb
@@ -1,13 +1,12 @@
 require_relative 'support/benchmark_bundler.rb'
 require 'bundler/cli'
 
-# Fixed Nokogiri version because latest version 1.6.8 fails to install
 
 Dir.mktmpdir do |dir|
   Dir.chdir(dir) do
     File.write('Gemfile', <<-GEMFILE)
       source 'https://rubygems.org'
-      gem 'rails'
+      gem 'rails', '5.0.0'
       gem 'nokogiri', '1.6.7.2'
     GEMFILE
 
