c0790fb4895b0ec831aa7ec7d4d26642b1f73d18
Guo Xiang Tan <tgx_world@hotmail.com>
2016-11-19 17:01:34 +0800

    Uncomment script.

--- optcarrot_benchmarks/driver.rb
+++ optcarrot_benchmarks/driver.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'digest'
+require_relative 'optcarrot/lib/optcarrot'
 
 ROM = 'Lan_Master.nes'
 
@@ -21,30 +23,30 @@ end
 avg_fps = results.inject{ |sum, el| sum + el }.to_f / results.size
 checksum = checksums.uniq.first
 
-# http = Net::HTTP.new(ENV["API_URL"] || 'rubybench.org')
-# request = Net::HTTP::Post.new('/benchmark_runs')
-# request.basic_auth(ENV["API_NAME"], ENV["API_PASSWORD"])
+http = Net::HTTP.new(ENV["API_URL"] || 'rubybench.org')
+request = Net::HTTP::Post.new('/benchmark_runs')
+request.basic_auth(ENV["API_NAME"], ENV["API_PASSWORD"])
 
-# initiator_hash = {}
-# if(ENV['RUBY_COMMIT_HASH'])
-#   initiator_hash['commit_hash'] = ENV['RUBY_COMMIT_HASH']
-# elsif(ENV['RUBY_VERSION'])
-#   initiator_hash['version'] = ENV['RUBY_VERSION']
-# end
+initiator_hash = {}
+if(ENV['RUBY_COMMIT_HASH'])
+  initiator_hash['commit_hash'] = ENV['RUBY_COMMIT_HASH']
+elsif(ENV['RUBY_VERSION'])
+  initiator_hash['version'] = ENV['RUBY_VERSION']
+end
 
-# request.set_form_data({
-#   'benchmark_result_type[name]' => 'Number of frames',
-#   'benchmark_result_type[unit]' => 'fps',
-#   'benchmark_type[category]' => "Optcarrot #{ROM}",
-#   'benchmark_type[script_url]' => "https://raw.githubusercontent.com/mame/optcarrot/master/lib/optcarrot/nes.rb",
-#   'benchmark_type[digest]' => '',
-#   "benchmark_run[result][default]" => avg_fps,
-#   'benchmark_run[environment]' => { "Ruby version" => `ruby -v`, "Checksum" => checksum },
-#   'repo' => 'ruby',
-#   'organization' => 'ruby'
-# })
+request.set_form_data({
+  'benchmark_result_type[name]' => 'Number of frames',
+  'benchmark_result_type[unit]' => 'fps',
+  'benchmark_type[category]' => "Optcarrot #{ROM}",
+  'benchmark_type[script_url]' => "https://raw.githubusercontent.com/mame/optcarrot/master/lib/optcarrot/nes.rb",
+  'benchmark_type[digest]' => Digest::SHA2.hexdigest(Optcarrot::VERSION),
+  "benchmark_run[result][default]" => avg_fps,
+  'benchmark_run[environment]' => { "Ruby version" => `ruby -v`, "Checksum" => checksum },
+  'repo' => 'ruby',
+  'organization' => 'ruby'
+})
 
-# http.request(request)
+http.request(request)
 puts "Posting results to Web UI...."
 puts "Average FPS: #{avg_fps}"
 puts "Checksum: #{checksum}"
