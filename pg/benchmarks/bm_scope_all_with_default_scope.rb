4e891b05d58d1c1d5789f4889c2772f17f04354c
bmarkons <mamaveb@gmail.com>
2017-05-16 13:53:26 +0000

    Add benchmark for scoped query

--- pg/benchmarks/bm_scope_all_with_default_scope.rb
+++ pg/benchmarks/bm_scope_all_with_default_scope.rb
@@ -8,14 +8,18 @@ db = PG::Helper.connect
 
 db.drop_and_create_table_users
 
-1000.times do
+500.times do
   db.insert_user
 end
 
-Benchmark.pg("raw_pg/postgres_scope_all", time: 5) do
+500.times do
+  db.insert_admin
+end
+
+Benchmark.pg("raw_pg/postgres_scope_all_with_default_scope", time: 5) do
   str = ""
-  db.all_users.each_row do |row|
-    str << "name: #{row[0]} email: #{row[1]}\n"
+  db.all_admins.each do |admin|
+    str << "name: #{admin["name"]} email: #{admin["email"]}\n"
   end
 end
 
