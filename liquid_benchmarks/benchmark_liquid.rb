1e2b683d52c025b5fa8b0583a3b514bb61a6d659
bmarkons <mamaveb@gmail.com>
2017-05-16 09:56:46 +0000

    Make common benchmark runner and call it with specific parameters

--- liquid_benchmarks/benchmark_liquid.rb
+++ liquid_benchmarks/benchmark_liquid.rb
@@ -1,44 +1,12 @@
 require 'benchmark/ips'
 require 'json'
 
+require_relative '../support/benchmark_runner'
+
 module Benchmark
   module Liquid
-    def liquid(label=nil, time: 10, warmup: 5, &block)
-      unless block_given?
-        raise ArgumentError.new, "block should be passed"
-      end
-
-      report = Benchmark.ips(time, warmup, true) do |x|
-        x.report(label) { yield }
-      end
-
-      entry = report.entries.first
-
-      output = {
-        label: label,
-        version: ::Liquid::VERSION.to_s,
-        iterations_per_second: entry.ips,
-        iterations_per_second_standard_deviation: entry.ips_sd,
-        total_allocated_objects_per_iteration: get_total_allocated_objects(&block)
-      }.to_json
-
-      puts output
-    end
-
-    def get_total_allocated_objects
-      if block_given?
-        key =
-          if RUBY_VERSION < '2.2'
-            :total_allocated_object
-          else
-            :total_allocated_objects
-          end
-
-        before = GC.stat[key]
-        yield
-        after = GC.stat[key]
-        after - before
-      end
+    def liquid(label=nil, version: ::Liquid::VERSION.to_s, time: 10, disable_gc: false, warmup: 5, &block)
+      Benchmark::Runner.run(label, version: version, time: time, disable_gc: disable_gc, warmup: warmup, &block)
     end
   end
 
--- sequel/benchmarks/support/benchmark_sequel.rb
+++ sequel/benchmarks/support/benchmark_sequel.rb
@@ -1,52 +1,13 @@
 require 'benchmark/ips'
 require 'json'
 
+require_relative '../../../support/benchmark_runner'
 require_relative 'helpers.rb'
 
 module Benchmark
   module Sequel
-    def sequel(label=nil, time:, disable_gc: true, warmup: 3, &block)
-      unless block_given?
-        raise ArgumentError.new, "block should be passed"
-      end
-
-      if disable_gc
-        GC.disable
-      else
-        GC.enable
-      end
-
-      report = Benchmark.ips(time, warmup, true) do |x|
-        x.report(label) { yield }
-      end
-
-      entry = report.entries.first
-
-      output = {
-        label: label,
-        version: ::Sequel.version.to_s,
-        iterations_per_second: entry.ips,
-        iterations_per_second_standard_deviation: entry.stddev_percentage,
-        total_allocated_objects_per_iteration: get_total_allocated_objects(&block)
-      }.to_json
-
-      puts output
-    end
-
-    def get_total_allocated_objects
-      if block_given?
-        key =
-          if RUBY_VERSION < '2.2'
-            :total_allocated_object
-          else
-            :total_allocated_objects
-          end
-
-        before = GC.stat[key]
-        yield
-        after = GC.stat[key]
-        after - before
-      end
+    def sequel(label=nil, version: ::Sequel.version.to_s, time:, disable_gc: true, warmup: 3, &block)
+      Benchmark::Runner.run(label, version: version, time: time, disable_gc: disable_gc, warmup: warmup, &block)
     end
   end
 
