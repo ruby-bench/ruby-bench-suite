1380d19abdd62d460f750d0cc678266d8eddb303
bmarkons <mamaveb@gmail.com>
2017-08-29 12:15:56 +0200

    Add pg/scope_all_over_select benchmark

--- pg/benchmarks/bm_scope_all_over_select.rb
+++ pg/benchmarks/bm_scope_all_over_select.rb
@@ -7,10 +7,10 @@ db_setup script: "bm_scope_all_setup.rb"
 connection = PG.connect(ENV.fetch("DATABASE_URL"))
 type_mapper = PG::BasicTypeMapForResults.new(connection)
 
-Benchmark.pg("pg/scope_all", time: 5) do
+Benchmark.pg("pg/scope_all_over_select", time: 5) do
   str = ""
 
-  result = connection.exec("SELECT name, email, approved, age, birthday FROM users")
+  result = connection.exec("SELECT * FROM users")
   result.type_map = type_mapper
   
   result.each_row do |row| 
--- support/setup/bm_scope_all_setup.rb
+++ support/setup/bm_scope_all_setup.rb
@@ -8,6 +8,9 @@ ActiveRecord::Migration.verbose = false
 ActiveRecord::Schema.define do
   create_table :users, force: true do |t|
     t.string :name, :email
+    t.boolean :approved
+    t.integer :age
+    t.datetime :birthday
     t.timestamps null: false
   end
 end
@@ -16,7 +19,10 @@ class User < ActiveRecord::Base; end
 
 attributes = {
   name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  email: "foobar@email.com"
+  email: "foobar@email.com",
+  approved: false,
+  age: 51,
+  birthday: DateTime.now
 }
 
 1000.times do
