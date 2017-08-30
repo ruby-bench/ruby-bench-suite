c8abe7e06d1296f991d5329d16052e9f4db65026
bmarkons <mamaveb@gmail.com>
2017-08-30 11:58:17 +0200

    Use while while loop instead of blocks in raw benchmarks

--- pg/benchmarks/bm_scope_all_over_select.rb
+++ pg/benchmarks/bm_scope_all_over_select.rb
@@ -12,9 +12,11 @@ Benchmark.pg("pg/scope_all_over_select", time: 5) do
 
   result = connection.exec("SELECT * FROM users")
   result.type_map = type_mapper
-  
-  result.each_row do |row| 
-    str << "name: #{row[0]} email: #{row[1]} approved: #{row[2]} age: #{row[3]} birthday: #{row[4]} \n"
+
+  i = 0
+  while i < result.ntuples do
+    str << "name: #{result.getvalue(i, 0)} email: #{result.getvalue(i, 1)} approved: #{result.getvalue(i, 2)} age: #{result.getvalue(i, 3)} birthday: #{result.getvalue(i, 4)} \n"
+    i += 1
   end
 end
 
