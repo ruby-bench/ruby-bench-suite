33edd04a760cf4807ef97d0b8848b375c09ba56e
bmarkons <mamaveb@gmail.com>
2017-08-31 15:02:04 +0200

    Fix sequel finders bench

--- sequel/benchmarks/bm_sequel_finders_find_by_attributes.rb
+++ sequel/benchmarks/bm_sequel_finders_find_by_attributes.rb
@@ -9,6 +9,6 @@ DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
 class User < Sequel::Model; end
 
 Benchmark.sequel("sequel/#{db_adapter}_finders_find_by_attributes", time: 5) do
-  user = User.select(:name, :email).find(email: 'shatrov@me.com')
+  user = User.select(:name, :email)[email: 'shatrov@me.com']
   str = "name: #{user.name} email: #{user.email}"
 end
