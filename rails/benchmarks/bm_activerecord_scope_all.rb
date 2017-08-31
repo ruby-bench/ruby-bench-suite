5b100f148abe40826744f4ad00ac267e45b86109
bmarkons <mamaveb@gmail.com>
2017-08-31 12:04:51 +0200

    Replace pluck with select in activerecord/scope_all

--- rails/benchmarks/bm_activerecord_scope_all.rb
+++ rails/benchmarks/bm_activerecord_scope_all.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base; end
 
 Benchmark.rails("activerecord/#{db_adapter}_scope_all", time: 5) do
   str = ""
-  User.pluck(:name, :email, :approved, :age, :birthday).each do |user|
+  User.select(:name, :email, :approved, :age, :birthday).each do |user|
     str << "name: #{user.name} email: #{user.email} approved: #{user.approved} age: #{user.age} birthday: #{user.birthday}\n"
   end
 end
