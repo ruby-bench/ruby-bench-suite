28da96ae4b98259ba1092a47f464c428805ee081
Guo Xiang Tan <tgx_world@hotmail.com>
2017-09-05 15:18:10 +0800

    Add benchmark for `ActiveRecord#pluck`.

---
+++ rails/benchmarks/bm_activerecord_pluck.rb
@@ -0,0 +1,28 @@
+require 'bundler/setup'
+require 'active_record'
+require_relative 'support/benchmark_rails'
+
+ActiveRecord::Base.establish_connection(ENV.fetch('DATABASE_URL'))
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name, :email
+    t.timestamps null: false
+  end
+end
+
+attributes = {
+  name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  email: 'foobar@email.com'
+}
+
+class User < ActiveRecord::Base; end
+
+1000.times do
+  User.create!(attributes)
+end
+
+Benchmark.rails("activerecord/#{db_adapter}_pluck", time: 5) do
+  User.all.pluck(:id, :name)
+end
