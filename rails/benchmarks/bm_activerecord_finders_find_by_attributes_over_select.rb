c5c607fbb0c0fe5555bc1b0bb038d72ff9566d1a
Guo Xiang Tan <tgx_world@hotmail.com>
2017-09-07 08:47:10 +0800

    `find_by_*` is going to be removed soon.

--- rails/benchmarks/bm_activerecord_finders_find_by_attributes_over_select.rb
+++ rails/benchmarks/bm_activerecord_finders_find_by_attributes_over_select.rb
@@ -10,6 +10,6 @@ ActiveRecord::Migration.verbose = false
 class User < ActiveRecord::Base; end
 
 Benchmark.rails("activerecord/#{db_adapter}_finders_find_by_attributes_over_select", time: 5) do
-  user = User.find_by_email('shatrov@me.com')
+  user = User.find_by(email: 'shatrov@me.com')
   str = "name: #{user.name} email: #{user.email}"
 end
