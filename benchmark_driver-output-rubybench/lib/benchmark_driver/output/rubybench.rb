6a0b7200bed85a73826a7cd4c323b9c52d6a8520
Takashi Kokubun <takashikkbn@gmail.com>
2018-07-01 00:04:32 +0900

    bundle gem benchmark_driver-output-rubybench

---
+++ benchmark_driver-output-rubybench/lib/benchmark_driver/output/rubybench.rb
@@ -0,0 +1,9 @@
+require "benchmark_driver/output/rubybench/version"
+
+module BenchmarkDriver
+  module Output
+    module Rubybench
+      # Your code goes here...
+    end
+  end
+end
---
+++ benchmark_driver-output-rubybench/lib/benchmark_driver/output/rubybench/version.rb
@@ -0,0 +1,7 @@
+module BenchmarkDriver
+  module Output
+    module Rubybench
+      VERSION = "0.1.0"
+    end
+  end
+end
