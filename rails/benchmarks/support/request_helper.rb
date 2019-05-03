c9881fab79b5284d7aae668e4a27ecb54a6cf1e3
Guo Xiang Tan <tgx_world@hotmail.com>
2019-05-03 16:14:45 +0800

    302 is valid too.

--- rails/benchmarks/support/request_helper.rb
+++ rails/benchmarks/support/request_helper.rb
@@ -1,7 +1,9 @@
 module RequestHelper
+  VALID_STATUS = [200, 302]
+
   def self.perform(app, request)
     status, _, body = app.call(request)
-    raise "Error: response status: #{status}" if status != 200
+    raise "Error: response status: #{status}" unless VALID_STATUS.include?(status)
     body.close if body.respond_to?(:close)
   end
 end
